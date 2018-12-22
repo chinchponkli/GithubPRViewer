cb1d3753aa8d6466cfc04e768a761479ecdf4e9f
arjit <arjita@zeta.tech>
2018-12-22 19:56:31 +0530

    Make owner and repo injectable

--- app/src/main/java/com/arjit/githubprviewer/di/module/ApplicationModule.kt
+++ app/src/main/java/com/arjit/githubprviewer/di/module/ApplicationModule.kt
@@ -2,9 +2,13 @@ package com.arjit.githubprviewer.di.module
 
 import android.app.Application
 import android.content.Context
+import com.arjit.githubprviewer.BuildConfig
 import com.arjit.githubprviewer.di.qualifier.ApplicationContext
+import com.arjit.githubprviewer.di.qualifier.Owner
+import com.arjit.githubprviewer.di.qualifier.Repo
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class ApplicationModule(private val application: Application) {
@@ -12,4 +16,14 @@ class ApplicationModule(private val application: Application) {
     @Provides
     @ApplicationContext
     fun provideApplicationContext(): Context = application
+
+    @Provides
+    @Singleton
+    @Owner
+    fun provideOwner(): String = BuildConfig.OWNER
+
+    @Provides
+    @Singleton
+    @Repo
+    fun provideRepo(): String = BuildConfig.REPO
 }
