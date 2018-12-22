7d1189a6869f2d683e98df0bcda13a8bc256f6d0
arjit <arjita@zeta.tech>
2018-12-22 21:48:43 +0530

    Add RecyclerView in fragment

--- app/src/main/java/com/arjit/githubprviewer/di/component/ActivityComponent.kt
+++ app/src/main/java/com/arjit/githubprviewer/di/component/ActivityComponent.kt
@@ -3,11 +3,14 @@ package com.arjit.githubprviewer.di.component
 import com.arjit.githubprviewer.di.module.ActivityModule
 import com.arjit.githubprviewer.di.scope.PerActivity
 import com.arjit.githubprviewer.ui.activity.PullRequestActivity
+import com.arjit.githubprviewer.ui.fragment.PullRequestFragment
 import dagger.Subcomponent
 
 @PerActivity
 @Subcomponent(modules = [(ActivityModule::class)])
 interface ActivityComponent {
+
+    fun inject(pullRequestFragment: PullRequestFragment)
 
     fun inject(pullRequestActivity: PullRequestActivity)
 }
--- app/src/main/java/com/arjit/githubprviewer/ui/fragment/PullRequestFragment.kt
+++ app/src/main/java/com/arjit/githubprviewer/ui/fragment/PullRequestFragment.kt
@@ -1,6 +1,15 @@
 package com.arjit.githubprviewer.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.arjit.githubprviewer.GitApplication
+import com.arjit.githubprviewer.R
+import com.arjit.githubprviewer.databinding.FragmentPullRequestBinding
+import com.arjit.githubprviewer.di.module.ActivityModule
 
 class PullRequestFragment : Fragment() {
 
@@ -8,5 +17,18 @@ class PullRequestFragment : Fragment() {
         val TAG = PullRequestFragment::class.java.simpleName
 
         fun newInstance() = PullRequestFragment()
+    }
+
+    private lateinit var binding: FragmentPullRequestBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+        GitApplication.applicationComponent!!.plus(ActivityModule(activity!!)).inject(this)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_pull_request, container, false)
+        return binding.root
     }
 }
